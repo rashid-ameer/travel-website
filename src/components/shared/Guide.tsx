ca6d6217d05272e48537688879c535c836a21374
Rashid Ameer <r.ameer.offical@gmail.com>
2024-03-22 21:35:42 +0500

    Fix Guide Image positioning

--- src/components/shared/Guide.tsx
+++ src/components/shared/Guide.tsx
@@ -20,15 +20,15 @@ function Guide() {
         </div>
       </div>
 
-      <div className="relative mt-20 2xl:rounded-2xl 2xl:container">
+      <div className="flex items-center justify-center relative mt-20 2xl:container">
         <Image
-          className="w-full aspect-[2.62]"
+          className="w-full object-center object-cover 2xl:rounded-2xl"
           src="/images/boat.png"
           alt="boat"
           width={1000}
           height={380}
         />
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:translate-x-0 md:translate-y-0 md:top-10 md:left-10 lg:top-20 lg:left-20 z-10 rounded-2xl pl-5 pr-7 py-8 w-[280px] flex gap-4 bg-white mx-auto md:mx-0 shadow-[0_0_10px_rgba(0,0,0,0.2)]">
+        <div className="flex pl-5 pr-7 py-8 rounded-xl gap-4 bg-white absolute md:left-[5%] lg:top-20 border shadow-[0_0_10px_rgba(0,0,0,0.2)]">
           <Image
             src="/images/meter.svg"
             alt=""
@@ -43,14 +43,12 @@ function Guide() {
                 <h3>Destination</h3>
                 <span className="text-green-100 font-bold">48min</span>
               </div>
-              <p className="mt-4 text-dark font-bold text-xl whitespace-nowrap">Aguas Calientes</p>
+              <p className="mt-3 text-dark font-bold text-xl whitespace-nowrap">Aguas Calientes</p>
             </div>
 
             <div>
-              <div className="flex justify-between">
-                <h3>Destination</h3>
-              </div>
-              <p className="text-dark font-bold text-xl whitespace-nowrap">Wonorejo Pasuruan</p>
+              <h3>Start Track</h3>
+              <p className="mt-3 text-dark font-bold text-xl whitespace-nowrap">Wonorejo Pasuruan</p>
             </div>
           </div>
         </div>
